f6edd70f7b7ccd6b7fba9ef349a51c57eff0a861
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2025-01-26 16:04:24 +0600

    Practice Password Text Field as I need!

--- app/src/main/java/com/jubayer_ahamad_tayef/password_text_field_practice_in_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/jubayer_ahamad_tayef/password_text_field_practice_in_jetpack_compose/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jubayer_ahamad_tayef.password_text_field_practice_in_jetpack_compose.ui.theme.Password_Text_Field_Practice_In_Jetpack_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +34,63 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Password_Text_Field_Practice_In_Jetpack_ComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyPasswordTextField()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyPasswordTextField() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var password by rememberSaveable {
+            mutableStateOf("")
+        }
+
+        var passwordVisibility by rememberSaveable {
+            mutableStateOf(false)
+        }
+
+        val icon =
+            if (passwordVisibility) painterResource(id = R.drawable.ic_visibility_24) else painterResource(
+                id = R.drawable.ic_visibility_off_24
+            )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Password") },
+            placeholder = {
+                Text(
+                    text = "Enter your Password here"
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_lock_24),
+                    contentDescription = "Lock Icon"
+                )
+            },
+            trailingIcon = {
+                IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                    Icon(
+                        painter = icon, contentDescription = "Visibility Icon"
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+            shape = RoundedCornerShape(15.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    Password_Text_Field_Practice_In_Jetpack_ComposeTheme {
-        Greeting("Android")
-    }
+fun MyPasswordTextFieldPreview() {
+    MyPasswordTextField()
 }
